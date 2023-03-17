8e7763cbb37fce17f599383176b8b08859c0edf5
hknhtpgl48 <hkn.hatipolu48@gmail.com>
2023-03-17 21:35:29 +0300

    if else statements

--- Patika/ViewController.swift
+++ Patika/ViewController.swift
@@ -1,19 +1,34 @@
-//
-//  ViewController.swift
-//  Patika
-//
-//  Created by Hakan Hatipoğlu on 17.03.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+    @IBOutlet weak var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        errorLabel.isHidden = true
     }
-
-
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        if usernameTextField.text == "hakan", passwordTextField.isHidden {
+            passwordTextField.isHidden = false
+            errorLabel.isHidden = true
+        } else {
+            if usernameTextField.text != "hakan" {
+                errorLabel.text = "Kullanıcı Adı Yanlış"
+                errorLabel.isHidden = false
+            } else if !(passwordTextField.isHidden), passwordTextField.text == "123456" {
+                errorLabel.isHidden = true
+                activityIndicatorView.startAnimating()
+            } else {
+                errorLabel.text = "Şifre Yanlış"
+                errorLabel.isHidden = false
+            }
+        }
+        
+    }
 }
 
